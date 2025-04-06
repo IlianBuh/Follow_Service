b5d06444c21f03de68e277bc614a663dc1ff5d7a
Your Name <you@example.com>
2025-04-06 10:55:49 +0300

    feat: finalize the preparations
     - init logger; add sl wrapper for errors
    
     - init config instance

---
+++ internal/config/config.go
@@ -0,0 +1,67 @@
+package config
+
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env        string  `yaml:"env" env-default:"prod"`
+	StorageURL string  `yaml:"storage-url" env-required:"true"`
+	GRPC       GRPCObj `yaml:"grpc"`
+}
+
+type GRPCObj struct {
+	Port    int           `yaml:"port" env-default:"20202"`
+	Timeout time.Duration `yaml:"timeout" env-default:"5s"`
+}
+
+const (
+	defaultConfigPath = "./config/config.yml"
+)
+
+// MustLoad returns new config object. Panics if error occurred.
+func MustLoad() *Config {
+	path := fetchConfigPath()
+
+	return MustLoadByPath(path)
+}
+
+// MustLoadByPath returns new config object by the 'path'. Panics if error occurred.
+func MustLoadByPath(path string) *Config {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file is not found: " + path)
+	}
+
+	var cfg Config
+	err := cleanenv.ReadConfig(path, &cfg)
+	if err != nil {
+		panic("failed to parse config file" + err.Error())
+	}
+
+	return &cfg
+}
+
+// fetchConfigPath fetches the config file path either from flag "config" or
+// environment variable "CONFIG_PATH". If no both of them, default path is returned
+//
+// flag > env > default
+func fetchConfigPath() string {
+	res := ""
+
+	flag.StringVar(&res, "config", "", "path to the config file")
+	flag.Parse()
+	if res != "" {
+		return res
+	}
+
+	res = os.Getenv("CONFIG_PATH")
+	if res != "" {
+		return res
+	}
+
+	res = defaultConfigPath
+	return res
+}
