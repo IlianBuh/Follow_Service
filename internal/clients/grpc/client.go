c51132d48e614cbb6058edcccf8815fc8fdce52c
Your Name <you@example.com>
2025-04-09 00:16:50 +0300

    feat: add to Client structure CheckUsers method

--- internal/clients/grpc/client.go
+++ internal/clients/grpc/client.go
@@ -1,10 +1,14 @@
 package grpclient
 
 import (
+	"context"
+	"fmt"
+	"github.com/IlianBuh/Follow_Service/internal/lib/mappers"
 	userinfov1 "github.com/IlianBuh/SSO_Protobuf/gen/go/userinfo"
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/retry"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
 	"log/slog"
 	"time"
 )
@@ -29,6 +33,7 @@ func New(
 
 	cc, err := grpc.NewClient(
 		addr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithChainUnaryInterceptor(
 			retry.UnaryClientInterceptor(retryOpts...),
 		),
@@ -43,4 +48,20 @@ func New(
 		log:       log,
 		gRPClient: gRPClient,
 	}, nil
+}
+
+func (c *Client) CheckUsers(ctx context.Context, uuids []int) (bool, error) {
+	const op = "grpclient.CheckUsers"
+
+	res, err := c.gRPClient.UsersExist(
+		ctx,
+		&userinfov1.UsersExistRequest{
+			Uuid: mappers.IntToInt32(uuids...),
+		},
+	)
+	if err != nil {
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return res.GetExist(), nil
 }
