6a6c90d92b7b4c0f36fe528884ffc73ea85b5589
Your Name <you@example.com>
2025-04-09 00:17:33 +0300

    feat: add grpc client initialization

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,10 +1,13 @@
 package app
 
 import (
+	"fmt"
 	grpcapp "github.com/IlianBuh/Follow_Service/internal/app/grpc"
+	grpclient "github.com/IlianBuh/Follow_Service/internal/clients/grpc"
 	"github.com/IlianBuh/Follow_Service/internal/service/follow"
 	"github.com/IlianBuh/Follow_Service/internal/storage/sqlite"
 	"log/slog"
+	"time"
 )
 
 type App struct {
@@ -14,14 +17,21 @@ type App struct {
 func New(
 	log *slog.Logger,
 	port int,
+	usrInfoPort int,
 	storagePath string,
+	retryCount int,
+	timeout time.Duration,
 ) *App {
 	st, err := sqlite.New(storagePath)
 	if err != nil {
 		panic(err)
 	}
 
-	fl := follow.New(log, st, st, st)
+	cl, err := grpclient.New(log, fmt.Sprintf(":%d", usrInfoPort), retryCount, timeout)
+	if err != nil {
+		panic(err)
+	}
+	fl := follow.New(log, st, st, st, cl)
 
 	application := grpcapp.New(log, port, fl)
 
